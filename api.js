cc50ddc9f9a2b0d89ea20cda9030798a1c515b89
jayvee-pearlpay <jaybee291998@gmail.com>
2023-01-14 07:14:20 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,2 +1,2 @@
-// let myapi_link = "https://attendancehub.pythonanywhere.com";
-let myapi_link = "http://127.0.0.1:8000";
+let myapi_link = "https://attendancehub.pythonanywhere.com";
+// let myapi_link = "http://127.0.0.1:8000";
