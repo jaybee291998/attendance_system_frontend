edc9cf4fee109bbcb2d0306738b6ecd73528ac64
jayvee-pearlpay <jaybee291998@gmail.com>
2023-01-18 19:10:10 +0800

    added grade and section to qr code showcase

--- home/controllers/QRController.js
+++ home/controllers/QRController.js
@@ -14,7 +14,13 @@
 			width : 300,
 			height : 300
 		});
+        let year_section = Authentication.getYearSection();
+        $scope.year_levels = year_section.year_levels;
+        $scope.sections = year_section.sections;
 
+        let year_level = $scope.year_levels.filter(year_level => year_level.id === parseInt($scope.user_profile.year_level))[0];
+        let section = $scope.sections.filter(section => section.id === parseInt($scope.user_profile.section))[0];
+        $scope.cred = `${year_level.name} - ${section.name}`;
         qrcode.makeCode($scope.user_profile.qr_code);
         // qrcode.makeCode("qrcode.makeCode($scope.user_profile.qr_code); lksajdfkljsldjalsjkdljkasldjkk");
     }
